e316181b83b24a46605886d56b0a0fe7ac9ab01b
Andrew Yoo <andrewyoo@andrewyoo-mbp.local>
2017-10-31 11:50:58 -0700

    fixed bug with facing mode

--- src/Camera/index.js
+++ src/Camera/index.js
@@ -461,7 +461,7 @@ export default class Camera extends Component {
 
         let constraints = {
             audio: false,
-            video: { width: 1920, height: 1080, acingMode: "environment" },
+            video: { width: 1920, height: 1080, facingMode: "environment" },
   
         }
        
